13bb0ff27a37fb83d41b3e560a48566bb2c6b414
Adithya Prasanth <adithya842@gmail.com>
2023-09-10 23:22:21 +0530

    Edits

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,15 +1,31 @@
+// import './App.css';
 import { useState } from 'react';
-// import @fontsource/inter;
-import './App.css';
+import { useColorScheme } from '@mui/joy/styles';
+import Button from '@mui/joy/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function ModeToggler() {
+  const { mode, setMode } = useColorScheme();
 
   return (
     <>
-      <h1>Personal Portfolio</h1>
+      
+      <Button
+        variant = "outlined"
+        color = "neutral"
+        onClick={() => setMode(mode === 'dark'? 'light' : 'dark')}
+      >
+        {mode === 'dark'? 'Light mode' : 'Dark mode'}
+      </Button>
     </>
+  );
+}
+
+function App() {
+  return(
+    <h1>Personal Portfolio</h1>
   )
+
 }
 
 export default App
