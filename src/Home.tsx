a915cf962167bc986ea283c7da835d818eb06c80
adithyay19 <adithya842@gmail.com>
2024-07-30 14:23:03 +0530

    Started homepage

---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return(
+        <div className="home" id="home">
+            <h2>Hello</h2>
+            <h1>Hi, I am Adithya Prasanth</h1>
+        </div>
+    );
+}
