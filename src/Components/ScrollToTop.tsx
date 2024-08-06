09484f8f610234f9554646bf73b72bdb759e8ae9
adithyay19 <adithya842@gmail.com>
2024-08-06 19:37:23 +0530

    Scroll to top feature added

--- src/Components/ScrollToTop.tsx
+++ src/Components/ScrollToTop.tsx
@@ -1,9 +1,33 @@
-import { Fab } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button"; // Or any other button library
+import { Fab } from "@mui/material";
 import NavigationIcon from '@mui/icons-material/Navigation';
 
-
 export default function ScrollToTop() {
-    <Fab onClick={() => {window.scrollTo({top:0, behavior: "smooth"})}}>
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 500) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <Fab onClick={handleClick} sx={{
+        display: showButton? 'block':'none',
+        position: "fixed",
+        bottom: 15,
+        right: 15
+    }}>
         <NavigationIcon />
     </Fab>
+  );
 }
